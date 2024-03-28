c2933e1fba050db8758deabd2bed4ba042474d65
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-03-28 15:11:27 +1100

    pre-litegraph submodule

--- src/ContentBase.js
+++ src/ContentBase.js
@@ -1,3 +1,16 @@
 class ContentBase {
+  _value = null;
+
   constructor() {}
+
+  update(data) {
+    setValue(data.datatype.value);
+  }
+
+  setValue(newValue) {
+    if (newValue !== this._value) {
+      const oldValue = this._value;
+      this._value = newValue;
+    }
+  }
 }
