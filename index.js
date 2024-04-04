2d46511e7d5d991b9afac3c96a1ea7930fe45950
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-04-04 13:36:11 +1100

    update route

--- index.js
+++ index.js
@@ -5,25 +5,42 @@ const app = express();
 const { AdsRpcClient } = require("mobject-client");
 
 const client = new AdsRpcClient("127.0.0.1.1.1", 851, "Main.server");
+// const client = new AdsRpcClient("192.168.4.1.1.1", 851, "Main.server");
 
 const litegraphPath = path.join(__dirname, "litegraph.js");
+const port = 8000;
 
-const port = 8000; // or any port you prefe
 app.use("/litegraph/css", express.static(path.join(litegraphPath, "css")));
 app.use("/litegraph/src", express.static(path.join(litegraphPath, "src")));
 app.use("/src", express.static("./src"));
 app.use("/", express.static("./public"));
-
 app.use(express.json());
 
-app.listen(port, () =>
-  console.log("Example app listening on http://127.0.0.1:8000")
-);
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to RPC server");
+    app.listen(port, () =>
+      console.log("Example app listening on http://127.0.0.1:" + port)
+    );
+  })
+  .catch((error) => {
+    console.error("Failed to connect to RPC server:", error);
+    process.exit(1);
+  });
+
+process.on("SIGINT", () => {
+  console.log("Disconnecting from RPC server...");
+  client.disconnect().then(() => {
+    console.log("Disconnected");
+    process.exit(0);
+  });
+});
 
 app.post("/rpc/:methodName", async (req, res) => {
   const methodName = req.params.methodName;
-
-  console.log(methodName, req.body);
+  console.log(methodName);
+  console.log(JSON.stringify(req.body));
   const params = req.body;
 
   if (typeof params !== "object" || params === null) {
@@ -31,14 +48,11 @@ app.post("/rpc/:methodName", async (req, res) => {
   }
 
   try {
-    await client.connect();
     const result = await client.rpcCall(methodName, params);
     res.json(result);
   } catch (error) {
     console.error(`Error calling RPC method ${methodName}:`, error);
     res.status(500).send(error);
-  } finally {
-    client.disconnect();
   }
 });
 
