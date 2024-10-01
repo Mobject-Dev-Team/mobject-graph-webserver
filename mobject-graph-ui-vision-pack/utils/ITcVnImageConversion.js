64ec02a878689a434d56c63f57a42ad2d18cd391
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-10-01 16:59:09 +1000

    added ability to show single channel images

--- mobject-graph-ui-vision-pack/utils/ITcVnImageConversion.js
+++ mobject-graph-ui-vision-pack/utils/ITcVnImageConversion.js
@@ -35,14 +35,15 @@ export function loadITcVnImageToImg(img, itcvnimage) {
   const binaryData = decodeBase64ToBinary(imageData);
 
   // Determine the correct bytes per pixel and channels
-  const { nChannels } = stPixelFormat;
+  const { nChannels, nElementSize } = stPixelFormat;
 
   // Process the binary data based on channel information
   const processedBytes = processImageData(
     binaryData,
     nWidth,
     nHeight,
-    nChannels
+    nChannels,
+    nElementSize
   );
 
   // Create and draw the image on the canvas
@@ -56,31 +57,51 @@ function decodeBase64ToBinary(base64String) {
   );
 }
 
-function processImageData(binaryData, width, height, channels) {
-  const bytes = new Uint8Array(width * height * 4);
+function processImageData(binaryData, width, height, channels, bitDepth) {
+  const bytesPerPixel = (channels * bitDepth) / 8;
+  const outputBytes = new Uint8Array(width * height * 4);
   let j = 0;
 
-  if (channels === 3) {
-    for (let i = 0; i < binaryData.length; i += 3) {
-      bytes.set([binaryData[i], binaryData[i + 1], binaryData[i + 2], 255], j);
-      j += 4;
+  for (let i = 0; i < binaryData.length; i += bytesPerPixel) {
+    let r,
+      g,
+      b,
+      a = 255; // Default alpha value
+
+    if (bitDepth === 8) {
+      if (channels === 1) {
+        r = g = b = binaryData[i];
+      } else if (channels === 3) {
+        r = binaryData[i];
+        g = binaryData[i + 1];
+        b = binaryData[i + 2];
+      } else if (channels === 4) {
+        r = binaryData[i];
+        g = binaryData[i + 1];
+        b = binaryData[i + 2];
+        a = binaryData[i + 3];
+      }
+    } else if (bitDepth === 16) {
+      if (channels === 1) {
+        r = g = b = (binaryData[i] + binaryData[i + 1] * 256) >> 8; // Assume little endian
+      } else if (channels === 3) {
+        r = (binaryData[i] + binaryData[i + 1] * 256) >> 8;
+        g = (binaryData[i + 2] + binaryData[i + 3] * 256) >> 8;
+        b = (binaryData[i + 4] + binaryData[i + 5] * 256) >> 8;
+      } else if (channels === 4) {
+        r = (binaryData[i] + binaryData[i + 1] * 256) >> 8;
+        g = (binaryData[i + 2] + binaryData[i + 3] * 256) >> 8;
+        b = (binaryData[i + 4] + binaryData[i + 5] * 256) >> 8;
+        a = (binaryData[i + 6] + binaryData[i + 7] * 256) >> 8;
+      }
     }
-  } else if (channels === 4) {
-    for (let i = 0; i < binaryData.length; i += 4) {
-      bytes.set(
-        [
-          binaryData[i],
-          binaryData[i + 1],
-          binaryData[i + 2],
-          binaryData[i + 3],
-        ],
-        j
-      );
-      j += 4;
-    }
+
+    // Set the RGBA values in the output buffer
+    outputBytes.set([r, g, b, a], j);
+    j += 4;
   }
 
-  return bytes;
+  return outputBytes;
 }
 
 function drawImageOnCanvas(imgElement, imageDataBytes, width, height) {
